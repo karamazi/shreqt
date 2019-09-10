72ec21e9d1be55b28bc2a4588c44934d016d6228
Zibi Rzepka <zibi667@gmail.com>
2019-09-10 17:16:14 +0100

    Extend example with temporary layer test

--- example/example.py
+++ example/example.py
@@ -5,4 +5,4 @@ def get_gb_emails():
     query = "SELECT email FROM utils.users_gb"
     with ExasolConnection().connect() as conn:
         rows = conn.execute(query).fetchall()
-        return [row["EMAIL"] for row in rows]
+        return sorted(row["EMAIL"] for row in rows)
--- example/example_test.py
+++ example/example_test.py
@@ -1,6 +1,19 @@
 from example.example import get_gb_emails
+from example.conftest import User
+from shreqt import LayerBuilder
 
 
 def test_get_gb_emails_returns_one_user():
     expected = ["joe@gmail.com"]
     assert get_gb_emails() == expected
+
+
+def test_get_gb_emails_can_handle_more_than_one_user(db_instance):
+    user_layer = LayerBuilder().with_model(User(email="kate@mail.gb", country="GB")).build()
+    expected_two = ["joe@gmail.com", "kate@mail.gb"]
+    with db_instance.layer(user_layer):
+        assert get_gb_emails() == expected_two
+
+    # Additional user should now be removed.
+    expected_one = ["joe@gmail.com"]
+    assert get_gb_emails() == expected_one
